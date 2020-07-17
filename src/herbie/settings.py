a057780f06fd7530ac986f4b743bc27db8b82646
Rui Ribeiro <rui.ribeiro@sennder.com>
2020-07-17 17:34:16 +0100

    remove old settings

--- src/herbie/settings.py
+++ src/herbie/settings.py
@@ -24,9 +24,6 @@ SCHEMA_REGISTRY_PACKAGE = 'schema'
 # chunk size for exporting data
 DEFAULT_CHUNK_SIZE = 100
 
-# message_provider (e.g. kafka, google pubsub)
-MESSAGING_PROVIDER = 'google_pubsub'
-
 # Google Cloud Pub/Sub
 GCLOUD_PUBSUB_PROJECT_ID = env.str('GCLOUD_PUBSUB_PROJECT_ID', " ")
 
@@ -78,22 +75,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'herbie.urls'
-
-
-SOCIAL_AUTH_URL_NAMESPACE = 'social'
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-    'herbieapp.authentication.social_authentication.process_roles'
-)
-
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
