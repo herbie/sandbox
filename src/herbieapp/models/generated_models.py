1f26cadb37d6f1820b23fd4c3ee783c0413e9352
tobias.rosenstock <tobi.rosenstock@gmail.com>
2020-07-29 11:21:18 +0200

    fix stuff after codereview

--- src/herbieapp/models/generated_models.py
+++ src/herbieapp/models/generated_models.py
@@ -1,5 +1,5 @@
 # generated file, should not be modified manually!
-from herbie_core.models import AbstractBusinessEntity
+from herbie_core.models.models import AbstractBusinessEntity
 
 
 class Customer(AbstractBusinessEntity):
